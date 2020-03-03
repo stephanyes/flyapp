115c1e5e377124b6e01c2ca0185d69d193d5a303
caroocat <karithofernandez@gmail.com>
2020-03-03 14:22:50 -0300

    primera branch voiler

--- back/db/db.js
+++ back/db/db.js
@@ -1,3 +1,6 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
-module.exports = new Sequelize('postgres://postgres:password@localhost:5432/omdb_db', { logging: false });
+module.exports = new Sequelize(
+  "postgres://postgres:password@localhost:5432/omdb_db",
+  { logging: false }
+);
