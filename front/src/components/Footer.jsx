64bb78cc66eb8d87139733e01a501f299cf3d603
caroocat <karithofernandez@gmail.com>
2020-03-03 19:14:32 -0300

    diseño principal

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
 export default () => (
-  <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="">
-        FLYAPP
-      </a>
-    </nav>
+  <div
+    style={{
+      left: "0",
+      bottom: "0",
+      width: "100%",
+      backgroundColor: "red",
+      color: "white",
+      textAlign: "center"
+    }}
+  >
+    <div className="row">
+      <div className="col">
+        <h4>All right reserved.</h4>
+      </div>
+      <div className="col"></div>
+
+      <div className="col"></div>
+      <div className="col">
+        <h5>FLYAPP</h5>
+      </div>
+    </div>
   </div>
 );
---
+++ front/src/containers/CartContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <div className="card text-center">
+      <div className="card-body">
+        <h1 className="card-title">Cart</h1>
+        <h4 className="card-text">These are your selected experiences.</h4>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-body">This is some text within a card body.</div>
+      <div className="card-body">This is some text within a card body.</div>
+      <div className="card-body">This is some text within a card body.</div>
+      <div className="card-body">This is some text within a card body.</div>
+      <div className="card-body">This is some text within a card body.</div>
+    </div>
+  </div>
+);
