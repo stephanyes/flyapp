4233ea117d2c6acf685e6e84850214984a0022d8
kravchukalex <alexk@insideone.com.ar>
2020-03-04 15:27:06 -0300

    mejoras de diseno

--- front/src/containers/NavbarContainer.jsx
+++ front/src/containers/NavbarContainer.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default () => (
   <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        FLYAPP
-      </a>
+    <nav
+      class="navbar navbar-expand-lg navbar-light"
+      style={{
+        backgroundColor: "#ffffff"
+      }}
+    >
+      <Link className="navbar-brand" to="/">
+        <img
+          src="https://insideone.s3-sa-east-1.amazonaws.com/flyapp-logo.png"
+          height="40"
+          alt=""
+        />
+      </Link>
+
       <button
         className="navbar-toggler"
         type="button"
@@ -41,12 +51,18 @@ export default () => (
             </Link>
           </li>
           <li className="nav-item active">
-            <Link className="nav-link" to="/cart">
-              Cart
+            <Link className="nav-link" to="/profile">
+              My Account
             </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#"></a>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/cart">
+              <img
+                src="https://insideone.s3-sa-east-1.amazonaws.com/buy-cart-black.png"
+                height="20"
+                alt=""
+              />
+            </Link>
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
@@ -56,12 +72,17 @@ export default () => (
             placeholder="Search"
             aria-label="Search"
           />
-          <button
+
+          {/* <button
             className="btn btn-outline-success my-2 my-sm-0"
+            style={{
+              color: "#2EC4B6",
+              borderColor: "#2EC4B6"
+            }}
             type="submit"
           >
             Search
-          </button>
+          </button> */}
         </form>
       </div>
     </nav>
