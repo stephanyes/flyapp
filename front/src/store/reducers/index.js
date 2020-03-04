7b9f59eec93cd8bfdd1b40902e4e28235ce24ea6
Agustin Fregossi <agustinfregossi@gmail.com>
2020-03-04 19:43:19 -0300

    search2.0

--- front/src/store/reducers/index.js
+++ front/src/store/reducers/index.js
@@ -3,10 +3,13 @@ import { combineReducers } from 'redux';
 import loginReducer from './loginReducer'
 import registerReducer from './registerReducer'
 import productsReducer from './productsReducer'
+import searchReducer from './searchReducer';
+
 
 
 export default combineReducers({
     userLogin: loginReducer,
     register: registerReducer,
-    productList: productsReducer
+    productList: productsReducer,
+    search_bar: searchReducer
 });
