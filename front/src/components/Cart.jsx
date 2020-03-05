9ace4ca5f96fd7898c34a7619a9aafe09040d6ff
kravchukalex <alexk@insideone.com.ar>
2020-03-05 20:52:04 -0300

    design of cart

--- front/src/components/Cart.jsx
+++ front/src/components/Cart.jsx
@@ -35,44 +35,40 @@ export default ({ carrito }) => {
         </div>
       </div>
 
-      <div
-        className="card mb-3"
-        style={{
-          borderColor: "#ffffff"
-        }}
-      >
-        <div className="row no-gutters">
-          {carrito ? (
-            <div className="div">
-              {carrito.data.map(algo => (
-                <div key={algo.id}>
-                  <div className="col-md-2">
-                    <img src={algo.img_1} className="card-img" alt="..." />
-
-                    <div className="col-md-10">
-                      <div className="card-body">
-                        <h5 className="font-weight-bold">{algo.name}</h5>
-                        <p className="font-weight-normal">{algo.description}</p>
-                        <p className="font-weight-bold">u$S {algo.price} </p>
-                        <Link
-                          className="btn btn-primary btn-lg"
-                          style={{
-                            backgroundColor: "#2EC4B6",
-                            borderColor: "#2EC4B6"
-                          }}
-                          to="/cart"
-                        >
-                          Delete
-                        </Link>
-                      </div>
-                    </div>
+      {carrito ? (
+        <div
+          style={{
+            margin: "40px"
+          }}
+        >
+          {carrito.data.map(algo => (
+            <div className="card mb-3" key={algo.id}>
+              <div className="row no-gutters">
+                <div className="col-md-2">
+                  <img src={algo.img_1} className="card-img" alt="..." />
+                </div>
+                <div className="col-md-10">
+                  <div className="card-body">
+                    <h5 className="font-weight-bold">{algo.name}</h5>
+                    <p className="font-weight-normal">{algo.description}</p>
+                    <p className="font-weight-bold">u$S {algo.price} </p>
+                    <Link
+                      className="btn btn-primary btn-lg"
+                      style={{
+                        backgroundColor: "#2EC4B6",
+                        borderColor: "#2EC4B6"
+                      }}
+                      to="/cart"
+                    >
+                      Delete
+                    </Link>
                   </div>
                 </div>
-              ))}
+              </div>
             </div>
-          ) : null}
+          ))}
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
