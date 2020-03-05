f2e21b493c160ac0ce9404d32973938151fac235
caroocat <karithofernandez@gmail.com>
2020-03-05 14:41:43 -0300

    lo mismo

--- front/src/containers/productIDContainer.jsx
+++ front/src/containers/productIDContainer.jsx
@@ -4,13 +4,32 @@ import { connect } from "react-redux";
 import { fetchSingleProduct } from "../store/actions/products";
 
 class ProductIDContainer extends React.Component {
+  constructor() {
+    super();
+    this.storingLocal = this.storingLocal.bind(this);
+  }
   componentDidMount() {
     this.props.seteador(this.props.match.params.id);
     console.log(this.props);
   }
+
+  storingLocal(e, producto) {
+    e.preventDefault();
+    console.log(producto);
+    let algo = JSON.parse(window.localStorage.getItem("testing"));
+    if (algo) {
+      window.localStorage.setItem(
+        "testing",
+        JSON.stringify([...algo, producto])
+      );
+    } else {
+      window.localStorage.setItem("testing", JSON.stringify([producto]));
+    }
+  }
+
   render() {
     const { product } = this.props;
-    return <Product producto={product} />;
+    return <Product producto={product} storing={this.storingLocal} />;
   }
 }
 
