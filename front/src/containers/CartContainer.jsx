c7c3c81db9757f62dd1ff86ad68507d9b8af192b
kravchukalex <alexk@insideone.com.ar>
2020-03-09 18:20:08 -0300

    screen categories-admin

--- front/src/containers/CartContainer.jsx
+++ front/src/containers/CartContainer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Cart from "../components/Cart";
+import Categories from "../components/Categories";
 import { connect } from "react-redux";
 import Axios from "axios";
 import { removeProduct } from "../store/actions/cart";
@@ -15,7 +16,7 @@ const mapStateToProps = state => {
 // const mapDispatchToProps = function(dispatch, ownProps) {
 //   return {
 //     eliminar: (newCart) => dispatch(removeProduct(newCart)),
-   
+
 //   };
 // };
 
@@ -25,49 +26,54 @@ class CartContainer extends React.Component {
     this.state = {
       products: []
     };
-    this.handleClick=this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
-    if(this.props.state.userLogin.loginUser){
-    Axios.get("/cart/products")
-    Axios.get("/cart/products")
-    .then(algo => algo.data)
-    .then(algo => {this.setState({ products: algo })})
+    if (this.props.state.userLogin.loginUser) {
+      Axios.get("/cart/products");
+      Axios.get("/cart/products")
+        .then(algo => algo.data)
+        .then(algo => {
+          this.setState({ products: algo });
+        });
     }
   }
-    
-  componentWillMount(){
-    if(this.props.state.userLogin.loginUser){
+
+  componentWillMount() {
+    if (this.props.state.userLogin.loginUser) {
+      Axios.get("/cart/products");
       Axios.get("/cart/products")
-      Axios.get("/cart/products")
-      .then(algo => algo.data)
-      .then(algo => {this.setState({ products: algo })})
-      }
-  //   let data = Object.assign({}, localStorage)
-  //   let prod = Object.values(data)
-  //   let producto = prod.map((e)=> JSON.parse(e))
-  //   this.setState({ products:producto})
+        .then(algo => algo.data)
+        .then(algo => {
+          this.setState({ products: algo });
+        });
+    }
+    //   let data = Object.assign({}, localStorage)
+    //   let prod = Object.values(data)
+    //   let producto = prod.map((e)=> JSON.parse(e))
+    //   this.setState({ products:producto})
   }
 
-  handleClick(e){
-    const logueado=this.props.state.userLogin.loginUser
-    if(!logueado){
-    let id = e.toString()
-    window.localStorage.removeItem(id)
-    window.location.reload()
+  handleClick(e) {
+    const logueado = this.props.state.userLogin.loginUser;
+    if (!logueado) {
+      let id = e.toString();
+      window.localStorage.removeItem(id);
+      window.location.reload();
     } else {
-      Axios.post("/cart/delete", {e})
-      window.location.reload()
+      Axios.post("/cart/delete", { e });
+      window.location.reload();
     }
     // let newCart = this.props.state.cart.cart;
     // for(let i=0;i<newCart.length;i++){
     //   if (newCart[i].id == e){newCart.splice(i, 1)}}
     // this.props.eliminar(newCart)
-  }  
+  }
 
   render() {
-    const logueado=this.props.state.userLogin.loginUser
-    return <Cart loged={logueado} handleClick={this.handleClick} carrito={this.state.products} cart={this.props.state.cart.cart} />;
+    const logueado = this.props.state.userLogin.loginUser;
+    //return <Cart loged={logueado} handleClick={this.handleClick} carrito={this.state.products} cart={this.props.state.cart.cart} />;
+    return <Categories />;
   }
 }
 
