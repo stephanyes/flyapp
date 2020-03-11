49e621ef96f11e48ca21eef802acc1e3c6142ff2
Agustin Fregossi <agustinfregossi@gmail.com>
2020-03-11 11:26:47 -0300

    fixx con alex

--- front/src/store/reducers/constants.js
+++ front/src/store/reducers/constants.js
@@ -2,10 +2,6 @@
 export const USER_LOGIN = "USER_LOGIN";
 export const USER_LOGOUT = "USER_LOGIN";
 export const RECEIVE_USERS = "RECEIVE_USERS";
-<<<<<<< HEAD
-
-=======
->>>>>>> df81dd83c838308851df92bf7ad7778a49b0aea3
 //Movies
 export const RECEIVE_MOVIES = "RECEIVE_MOVIES";
 export const RECEIVE_MOVIE = "RECEIVE_MOVIE";
@@ -21,13 +17,5 @@ export const FILL_CART = "FILL_CART";
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
 
 //Category
-<<<<<<< HEAD
 export const GET_CATEGORY = "GET_CATEGORY";
 export const GET_CATEGORYID = "GET_CATEGORYID";
-=======
-export const GET_CATEGORY = "GET_CATEGORY"
-export const GET_CATEGORYID = 'GET_CATEGORYID'
-
-
-
->>>>>>> df81dd83c838308851df92bf7ad7778a49b0aea3
